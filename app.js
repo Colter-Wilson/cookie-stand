d3fe71a44989defac8ef992385dd13467a544e6e
Colter-Wilson <itscolter@gmail.com>
2022-11-01 02:10:01 -0400

    cities and store hours

---
+++ app.js
@@ -0,0 +1,51 @@
+// #pragma GLOBAL VARIABLES + WINDOWS INTO DOM
+let storeHours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
+
+// #pragma HELPER FUNCTIONS - UTILITIES
+import random from math
+
+// #pragma OBJECT LITERALS
+
+let Seattle = {
+  total: 0,
+  name: 'Seattle',
+  minCust: 23,
+  maxCust: 65,
+  avgCookieSale: 6.3;
+}
+
+let Tokyo = {
+  total: 0,
+  name: 'Tokyo',
+  minCust: 3,
+  maxCust: 24,
+  avgCookieSale: 1.2;
+}
+
+let Dubai = {
+  total: 0,
+  name: 'Dubai',
+  minCust: 11,
+  maxCust: 38,
+  avgCookieSale: 3.7;
+}
+
+let Paris = {
+  total: 0,
+  name: 'Paris',
+  minCust: 20,
+  maxCust: 38,
+  avgCookieSale: 2.3;
+}
+
+let Lima = {
+  total: 0,
+  name: 'Lima',
+  minCust: 2,
+  maxCust: 16,
+  acvCookieSale: 4.6;
+}
+
+// #pragma EXECUTABLE CODE
+
+
